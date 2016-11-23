221558dc259e11d2a9748db7d96e83eee9e22a86
Hubert-Banaszewski <hubertban1@gmail.com>
2016-11-23 23:58:22 +0100

    Add files via upload

--- citizen.h
+++ citizen.h
@@ -1,6 +1,7 @@
 #ifndef CITIZEN_H
 #define CITIZEN_H
 
+#include <assert.h>
 
 // base classes
 
@@ -61,5 +62,73 @@ using Teenager = Citizen<T, 11, 17, defence::Vulnerable>;
 
 template<typename T>
 using Sheriff = Citizen<T, 18, 100, defence::Armed>;
+
+
+
+
+// IMPLEMENTATION
+
+
+// mortal
+template <typename T>
+T MortalBeing<T>::getHealth() const {
+    return health;
+}
+
+template <typename T>
+void MortalBeing<T>::takeDamage(const T damage) {
+    health -= health >= damage ? damage : health;
+}
+
+template <typename T>
+MortalBeing<T>::MortalBeing(const T health): health(health) {
+}
+
+// aged
+
+template <typename T, T minAge, T maxAge>
+void AgedBeing<T, minAge, maxAge>::age_assert() {
+    assert(age <= maxAge && age >= minAge);
+}
+
+template <typename T, T minAge, T maxAge>
+T AgedBeing<T, minAge, maxAge>::getAge() const {
+    return age;
+}
+
+template <typename T, T minAge, T maxAge>
+AgedBeing<T, minAge, maxAge>::AgedBeing(const T age): age(age) {
+    age_assert();
+}
+
+// damaging
+
+template <typename T>
+T DamagingBeing<T>::getAttackPower() const {
+    return damage;
+}
+
+template <typename T>
+DamagingBeing<T>::DamagingBeing(const T damage): damage(damage) {
+}
+
+
+// citizens
+
+template <typename T, T minAge, T maxAge, defence defType>
+Citizen<T, minAge, maxAge, defType>::Citizen(const T health, const  T age):
+    MortalBeing<T>(health),
+    AgedBeing<T, minAge, maxAge>(age) {
+}
+
+
+template <typename T, T minAge, T maxAge>
+Citizen<T, minAge, maxAge, defence::Armed>::Citizen(const T health, const T age, const T attackPower):
+    MortalBeing<T>(health),
+    AgedBeing<T, minAge, maxAge>(age),
+    DamagingBeing<T>(attackPower) {
+}
+
+
 
 #endif // CITIZEN_H
--- monster.h
+++ monster.h
@@ -4,15 +4,15 @@
 #include <string>
 #include "citizen.h"
 
-enum class beastType {
+enum class monsterType {
     Mummy,
     Zombie,
     Vampire
 };
 
-std::string beastTypeToString(beastType b);
+std::string monsterTypeToString(monsterType b);
 
-template <typename T, beastType b>
+template <typename T, monsterType b>
 class Monster: public MortalBeing<T>, public DamagingBeing<T> {
 public:
     Monster(const T health, const T attackPower);
@@ -22,13 +22,13 @@ public:
 };
 
 template<typename T>
-using Vampire = Monster<T, beastType::Vampire>;
+using Vampire = Monster<T, monsterType::Vampire>;
 
 template<typename T>
-using Zombie = Monster<T, beastType::Zombie>;
+using Zombie = Monster<T, monsterType::Zombie>;
 
 template<typename T>
-using Mummy = Monster<T, beastType::Mummy>;
+using Mummy = Monster<T, monsterType::Mummy>;
 
 template<typename M, typename U>
 void attack(const M& monster, U& victim);
@@ -36,4 +36,39 @@ void attack(const M& monster, U& victim);
 template<typename M, typename T>
 void attack(M& monster, Sheriff<T>& victim);
 
+
+
+
+// IMPLEMENTATION
+
+
+template <typename T, monsterType b>
+Monster<T, b>::Monster(const T health, const T attackPower): MortalBeing<T>(health), DamagingBeing<T>(attackPower) {
+}
+
+
+inline std::string monsterTypeToString(monsterType b) {
+    switch (b) {
+    case monsterType::Mummy: return "Mummy";
+    case monsterType::Zombie: return "Zombie";
+    case monsterType::Vampire: return "Vampire";
+    default: exit(1);
+    }
+}
+
+template <typename T, monsterType b>
+std::string Monster<T, b>::getName() const {
+    return monsterTypeToString(s);
+}
+
+template <typename M, typename U>
+void attack(const M& monster, U& victim) {
+    victim.takeDamage(monster.getAttackPower());
+}
+
+template <typename M, typename T>
+void attack(M& monster, Sheriff<T>& rival) {
+    rival.takeDamage(monster.getAttackPower());
+    monster.takeDamage(rival.getAttackPower());
+}
 #endif // MONSTER_H
