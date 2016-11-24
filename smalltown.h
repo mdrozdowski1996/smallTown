195f7bcc2b637ec83a33903421f3d597d2e2951c
Hubert-Banaszewski <hubertban1@gmail.com>
2016-11-24 01:53:12 +0100

    Add files via upload

--- smalltown.h
+++ smalltown.h
@@ -1,15 +1,22 @@
 #ifndef SMALLTOWN_H
 #define SMALLTOWN_H
+
 #include <vector>
+#include <cstdio>
+#include <algorithm>
+#include "monster.h"
 
 template <typename M, typename U, U t0, U t1, typename... C>
 class SmallTown {
     static_assert(t0 <= t1, "Wrong times");
-    std::vector<C> citizens;
-    M monster;
+
+    std::tuple<C...> citizens;
     std::vector<int> fibonacci;
+
+    M monster;
     U actTime;
 public:
+
     SmallTown(M monster, C ... citizens);
     template<typename T>
     std::tuple<std::string, T, size_t> getStatus();
@@ -18,25 +25,45 @@ public:
 
 
 
+template <typename M, typename U, U t0, U t1, typename ... C>
+SmallTown<M, U, t0, t1, C...>::SmallTown(M monster, C... citizens) : citizens(citizens), monster(monster), actTime(t0) {}
+
+
+template<typename M, typename U, U t0, U t1, typename ...C>
+void SmallTown<M, U, t0, t1, C...>::tick(U timeStep) {
+    bool allDead = true;
+
+    for (auto it : citizens) {
+        if (it.getHealth() > 0 ) {
+            allDead = false;
+            break;
+        }
+    }
+
+    if (monster.getHealth == 0 && allDead) {
+        printf("DRAW\n");
+    }
+    else if (monster.getHealth == 0) {
+        printf("CITIZENS WON\n");
+    }
+    else if (allDead) {
+        printf("MONSTER WON\n");
+    }
+
+    if (std::find(fibonacci.begin(), fibonacci.end(), actTime) != fibonacci.end()) {
+        for (auto it : citizens) {
+            attack(monster, it); // monster attacks simultaneously
+        }
+    }
+
+    actTime += timeStep;
+    actTime %= t1;
+}
+
+template <typename M, typename U, U t0, U t1, typename ... C>
+template <typename T>
+std::tuple<std::string, T, size_t> SmallTown<M, U, t0, t1, C...>::getStatus() {
+    return {monster.getName(), monster.getHealth(), citizens.size()};
+}
 
 #endif
-/*
-void tick(U timeStep) – na pocz¹tku sprawdza aktualny czas; jeœli jest to
-czas ataku, to nastêpuje atak na wszystkich mieszkañców; na koniec czas
-w miasteczku przesuwa siê o timeStep.
-
-SmallTown operuje zgodnie z wewnêtrznym zegarem.Czas liczony jest w godzinach,
-od godziny 0 do godziny t1 i potem znów od godziny 0, i tak cyklicznie.Pierwsze
-odliczanie zaczyna siê od godziny t0.Potwór atakuje tylko o godzinach równych
-kolejnym liczbom z ci¹gu Fibonacciego.Obliczenie wszystkich potrzebnych liczb
-z tego ci¹gu i ich zapamiêtanie w jakiejœ kolekcji musi odbyæ siê w czasie
-kompilacji.
-
-Œmieræ mieszkañca b¹dŸ potwora nastêpuje w momencie, gdy jego liczba punktów
-¿ycia osi¹gnie zero.Wywo³anie tick() na miasteczku z martwym potworem i co
-najmniej jednym ¿ywym mieszkañcem powoduje wypisanie na standardowe wyjœcie
-komunikatu "CITIZENS WON".Wywo³anie tick() na miasteczku z martwymi
-mieszkañcami i ¿ywym potworem powoduje wypisanie na standardowym wyjœciu napisu
-"MONSTER WON".Jeœli zarówno mieszañcy, jak i potwór s¹ martwi, to tick()
-wypisuje na standardowe wyjœcie "DRAW".
-*/
